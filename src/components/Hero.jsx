a3639d7e2a092238410237f137e0b60ba368c3fe
Yogesh <awesomeyogi10@gmail.com>
2023-08-26 02:31:04 +0530

    added 3d model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,22 @@ import {ComputersCanvas} from './canvas'
 const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+           Greetings fellow humans, My name is <span className='text-[#915EFF]'>Yogesh</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I am Computer Engineering student in my 4th year, <br className='sm:block hidden' />
+       currently looking for internship opportunities.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
       </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import {Suspense, useEffect, useState} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import  CanvasLoader from '../Loader'
 const Computers = () => {
+  const computer=useGLTF('./PC/scene.gltf')
   return (
-    <div>Computers</div>
+   
+<mesh>
+  <hemisphereLight intensity={0.35} groundColor="black" />
+  <pointLight position={[0, 50, 0]} intensity={0.5} />
+  <primitive object={computer.scene} scale={1.5} position={[0,0,0]} />
+</mesh>
+    
+  )
+}
+const ComputersCanvas = () => {
+  return(
+    <Canvas
+    frameloop='demand' shadows camera={{position:[20,3,5],fov:25}}
+    gl={{preserveDrawingBuffer:true}} >
+<Suspense fallback={<CanvasLoader />}>
+  <OrbitControls enableZoom={false}
+  maxPolarAngle={Math.PI/2}
+  minPolarAngle={Math.PI/2}/>
+  <Computers />
+  </Suspense>
+  <Preload all />
+    </Canvas>
   )
 }
 
-export default Computers
+export default ComputersCanvas
