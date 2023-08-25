3db564947ecb61d165d4471e0ff2d59333bd5af6
Yogesh <awesomeyogi10@gmail.com>
2023-08-25 22:03:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router} from 'react-router-dom'
+import {About, Contact, Experience, Feedbacks, Hero, Navbar,
+Tech, Works, StarsCanvas} from './components'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-no-center">
+            <Navbar />
+            <Hero />
+            
+          </div>
+          <About />
+          <Experience />
+          <Tech />  
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
+
+
+        </div>
+      </Router>
+      </div>
+  )
+}
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  {styles} from '../styles'
+import {ComputersCanvas} from './canvas'
+
+const Hero = () => {
+  return (
+    <section className="relative w-full h-screen mx-auto">
+      </section>
+  )
+}
+
+export default Hero
