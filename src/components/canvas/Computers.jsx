a45e7c5641969f65a096b94bdd46b6d97e8f3a98
Yogesh <awesomeyogi10@gmail.com>
2023-08-27 20:12:02 +0530

    added about section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,19 +2,34 @@ import {Suspense, useEffect, useState} from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import  CanvasLoader from '../Loader'
-const Computers = () => {
+const Computers = ({isMobile}) => {
   const computer=useGLTF('./PC/scene.gltf')
   return (
    
 <mesh>
   <hemisphereLight intensity={0.35} groundColor="black" />
   <pointLight position={[0, 50, 0]} intensity={0.5} />
-  <primitive object={computer.scene} scale={1.1} position={[4,-2.5,5]} />
+  <primitive object={computer.scene} scale={isMobile?0.3:1.1} position={[4,-2.5,5]} />
 </mesh>
     
   )
 }
 const ComputersCanvas = () => {
+  
+  const [isMobile, setIsMobile] = useState(false)
+  useEffect(() => {
+  const mediaQuery = window.matchMedia("(max-width: 768px)")  
+    setIsMobile(mediaQuery.matches)
+    const handleMediaQueryChange = (e) => {
+      setIsMobile(e.matches)
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+},[])
+  
+  
   return(
     <Canvas
     frameloop='demand' shadows camera={{position:[20,3,5],fov:25}}
@@ -23,7 +38,7 @@ const ComputersCanvas = () => {
   <OrbitControls enableZoom={false}
   maxPolarAngle={Math.PI/2}
   minPolarAngle={Math.PI/2}/>
-  <Computers />
+  <Computers  isMobile={isMobile}/>
   </Suspense>
   <Preload all />
     </Canvas>
